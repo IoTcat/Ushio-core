c86b2072e189f450beeddee84f74c0b622b1e3e6
呓喵酱 <i@iotcat.me>
2019-06-24 21:51:51 +0800

    Update ini.sh

---
+++ ini/sh/ini.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+yum update -y
+
+# nginx
+yum install -y nginx
+systemctl enable nginx
+systemctl start nginx
+
+# gcc
+yum install -y gcc
+yum install -y gcc-c++
+yum install -y gdb
+yum install -y openssl
+
+# php
+rpm -ivh http://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/epel-release-7-11.noarch.rpm
+rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
+yum -y install php72w php72w-cli php72w-common php72w-devel php72w-embedded php72w-fpm php72w-gd php72w-mbstring php72w-mysqlnd php72w-opcache php72w-pdo php72w-xml
+systemctl start php-fpm
+systemctl enable php-fpm
+
+# go
+yum install -y golang
+
+# nodejs
+yum install -y nodejs
+npm install -g n
+npm install -g npm
+npm install express-generator -g
+npm i forever -g
+
+# python
+yum install epel-release -y
+yum install https://centos7.iuscommunity.org/ius-release.rpm -y
+yum install python36u -y
+yum install python36u-devel -y
+ln -s /bin/python3.6 /bin/python
+yum install python36u-pip -y
+ln -s /bin/pip3.6 /bin/pip3
+yum install -y python-pip
+pip install --upgrade pip
+
+# docker
+yum -y install docker
+systemctl enable docker
+systemctl start docker
+pip install --upgrade backports.ssl_match_hostname
+pip install docker-compose
+
+
